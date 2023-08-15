b998b76d5daa7a5473af96ff3ded2a930fe739c2
Atikorn <you@example.com264318241004-st@rmutsb.ac.th>
2023-08-15 11:48:47 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import react from 'react'
+//import ArrayMap from './array-map';
+//import ColumnItem from './column-item';
+//import RowItem from './row-item';
+//import LayoutItem from './Layout-item';
+//import Question from './Question';
+//import Image from './image';
+//import ButtonDemo from './button-demo';
+//import ButtonStateDemo from './button-state-demo';
+//import TextInputDemo from './text-input-demo';
+//import CalDemo from './calculate-demo';//
+//import CalDemo2 from './cal2';
+//import CalDemo3 from './cal3';
+import CalDemo4 from './cal4';
+export default function App() {
+  return (
+      //[<Question/>,<Image/>,<LayoutItem/>]
+      //<ButtonDemo/>
+      //<ButtonStateDemo/>
+      //<CalDemo/>
+      //<CalDemo2/>
+      //<CalDemo3/>
+      <CalDemo4/>
+  );
+}
